741c2b6e4f06fe717112e9f8f91e44bc1493bad3
Travis Jensen <travis@innerbrane.com>
2014-04-30 00:46:15 -0600

    TemplateLoader should have overridden load().
    There is no "load_template".

--- gyoko/loader.py
+++ gyoko/loader.py
@@ -3,10 +3,10 @@ from jinja2 import FileSystemLoader
 
 
 class TemplateLoader(FileSystemLoader):
-    fname_extn = '.jinja'
+    template_extn = '.jinja'
 
-    def load_template(self, template_name, template_dirs=None):
-        if not template_name.endswith(self.fname_extn):
-            template_name = template_name + self.fname_extn
-        return super(TemplateLoader, self).load_template(template_name,
-                                                         template_dirs)
+    def load(self, env, template_name, globals=None):
+        if not template_name.endswith(self.template_extn):
+            template_name = template_name + self.template_extn
+        return super(TemplateLoader, self).load(
+            env, template_name, globals)
