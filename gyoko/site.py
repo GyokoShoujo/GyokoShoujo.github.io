2304c3d0e6a111cf22cfd2d86768b815c23fd446
Travis Jensen <travis@innerbrane.com>
2014-05-10 22:28:39 -0600

    Pre-compute chapter/page information.
    We need to know cross-chapter information for proper page
    navigation, so run through the directories first to build
    a map of chapters and pages, then generate the output. This
    allows us to always know the "next" page, even at the end of
    a chapter.

--- gyoko/site.py
+++ gyoko/site.py
@@ -1,3 +1,4 @@
+from collections import namedtuple
 import pathlib
 import shutil
 
@@ -12,6 +13,10 @@ from .loader import TemplateLoader
 from .log import message, info, debug
 
 env = None
+
+Chapter = namedtuple('Chapter', ['dir', 'pages', 'number', 'title',
+                                 'slug', 'markdown_file', ])
+Page = namedtuple('Page', ['source', 'number', 'title', 'markdown_file', ])
 
 
 def gen_site(source_dir):
@@ -31,90 +36,124 @@ def gen_site(source_dir):
     global env
     env = Environment(loader=TemplateLoader(str(tmpl_dir.resolve())))
 
+    chapters = build_chapters(content_dir)
+    info("Found {0} chapters.", len(chapters))
+
     # Copy static files
     static_source = source / 'static'
     static_dest = dest / 'static'
     info("Copying static files from {0} to {1}", static_source, static_dest)
     shutil.copytree(str(static_source.resolve()), str(static_dest))
     gen_dir_from_markdown(content_dir, dest, ignore_set={'index', 'toc', })
-    gen_images_dirs(content_dir, dest)
+    gen_chapters(chapters, dest)
+    # TODO: Make the core index.
 
     # TODO: generate the blog
     # gen_dir_from_markdown(source / "blog", dest, generic_tmpl_name='blog.jinja',
     #                       ignore=['index', ])
 
 
-def gen_images_dirs(source, output):
-    for d in source.iterdir():
-        if not d.is_dir():
+def build_chapters(source):
+    '''
+    Runs through the source directory and generates a map of all of
+    the pages. We return a list of chapters with all of the pages
+    each chapter contains. See the definitions of the namedtuples Page
+    and Chapter above.
+    '''
+    chapters = []
+    chapter_titles = set()
+    for chapter_dir in source.iterdir():
+        if not chapter_dir.is_dir():
             continue
-        parts = d.name.split('-', 1)
+        parts = chapter_dir.name.split('-', 1)
         if len(parts) == 1:
             info("Found non-matching directory {0}", source)
             continue
         chapter_num, title = (x.strip() for x in parts)
-        slug = slugify(title)
-        gen_dir_from_images(d, output, slug, title)
-        # TODO: gather the chapters up and make the core index.
+        if title in chapter_titles:
+            message('Duplicate chapter title: {0}.'
+                    .format(title))
+            message('Chapter title must be unique. Exiting.')
+            raise GyokoException()
+        chapter_titles.add(title)
+        md_file = chapter_dir.parent / 'index.md'
+        chapter = Chapter(chapter_dir, [], int(chapter_num),
+                          title, slugify(title), md_file)
+        page_title = title
+        for img in chapter_dir.glob('*.png'):
+            parts = img.name.split('-', 1)
+            if len(parts) == 2:
+                img_num, page_title = (x.strip() for x in parts)
+            else:
+                img_num = parts[0].strip()
+            md_file = img.parent / (img.stem + '.md')
+            chapter.pages.append(Page(img, int(img_num), page_title, md_file))
+        if len(chapter.pages) == 0:
+            info('No images found for chapter {0}', chapter.title)
+
+        chapter.pages.sort(key=lambda p: p.number)
+        chapters.append(chapter)
+
+    chapters.sort(key=lambda c: c.number)
+    return chapters
 
 
-def gen_dir_from_images(source, output, slug, chapter_title):
+def gen_chapters(chapters, output):
     '''
-    Generates a "chapter" of images from the given source directory.
+    Outputs the HTML for each chapters and the pages they contain.
     '''
-    html_dir = output / slug
-    img_dir = output / 'static' / slug / 'page-images'
-    thumbnail_dir = output / 'static' / slug / 'page-thumbnails'
+    page_count = sum([len(c.pages) for c in chapters])
+    page_num = 1
+    prev_url = None
+    for chapter_idx, chapter in enumerate(chapters):
+        html_dir = output / chapter.slug
+        img_dir = output / 'static' / chapter.slug / 'page-images'
+        thumbnail_dir = output / 'static' / chapter.slug / 'page-thumbnails'
 
-    for dir in (html_dir, img_dir, thumbnail_dir):
-        dir.mkdir(parents=True)
-    page_title = chapter_title
-    page_count = 0
-    pages = {}
-    url_base = '//static/' + slug + '/page-images'
-    thumb_base = '//static/' + slug + '/page-thumbnails'
-    for img in source.glob('*.png'):
-        parts = img.name.split('-', 1)
-        if len(parts) == 2:
-            img_num, page_title = (x.strip() for x in parts)
-        else:
-            img_num = parts[0].strip()
-        page_base = 'page-{0}'.format(img_num)
-        img_name = page_base + '.png'
-        thumbnail = thumbnail_dir / img_name
-        img_dest = img_dir / img_name
-        shutil.copy2(str(img.resolve()), str(img_dest))
-        make_thumbnail(img, thumbnail)
-        md_file = img.parent / (img.stem + '.md')
-        page_count = max(int(img_num), page_count)
+        for dir in (html_dir, img_dir, thumbnail_dir):
+            dir.mkdir(parents=True)
+
+        img_base = '//static/' + chapter.slug + '/page-images'
+        thumb_base = '//static/' + chapter.slug + '/page-thumbnails'
+        for page_idx, page in enumerate(chapter.pages):
+            page_base = 'page-{0}'.format(page.number)
+            img_name = page_base + '.png'
+            thumbnail = thumbnail_dir / img_name
+            img_dest = img_dir / img_name
+            shutil.copy2(str(page.source.resolve()), str(img_dest))
+            make_thumbnail(page.source, thumbnail)
+            page_url = '//{0}/{1}.html'.format(chapter.slug, page_base)
+
+            next_page = None
+            if page_idx < len(chapter.pages):
+                next_page = '//{0}/page-{1}.html'.format(chapter.slug, page_idx+1)
+            elif (chapter_idx < len(chapters) and
+                  len(chapters[chapter_idx+1].pages) > 0):
+                next_page = '//{0}/page-0.html'.format(chapters[chapter_idx+1].slug)
+            context = {
+                'page': page,
+                'page_title': page.title,
+                'page_image': img_base + '/' + img_name,
+                'has_previous': prev_url is not None,
+                'page_previous': prev_url,
+                'has_next': next_page is not None,
+                'page_next': next_page,
+                'page_number': page_num,
+                'page_count': page_count,
+            }
+            gen_html(page_base, html_dir, page.markdown_file,
+                     generic_tmpl_name='page', extra_context=context)
+            prev_url = page_url
+            page_num += 1
+
         context = {
-            'page_title': page_title,
-            'page_image': url_base + '/' + img_name
+            'chapter': chapter,
+            'page_title': chapter.title,
+            'file_base': 'page',
+            'thumbnail_base': thumb_base,
         }
-        pages[int(img_num)] = (page_base, md_file, context)
-
-    for img_num in sorted(pages.keys()):
-        page_base, markdown_file, context = pages[img_num]
-        context['has_previous'] = img_num > 0
-        context['has_next'] = img_num < (len(pages) - 1)
-        context['page_previous'] = url_base + '/page-' + str(img_num - 1) + '.html'
-        context['page_next'] = url_base + '/page-' + str(img_num + 1) + '.html'
-        gen_html(page_base, html_dir, markdown_file, generic_tmpl_name='page',
-                 extra_context=context)
-
-    if page_count == 0:
-        info('No images found in {0}', source)
-
-    context = {
-        'page_title': chapter_title,
-        'page_count': page_count,
-        'file_base': 'page',
-        'page_base': url_base,
-        'thumbnail_base': thumb_base,
-    }
-    index_md = source / 'index.md'
-    gen_html('index', html_dir, index_md, generic_tmpl_name='chapter',
-             extra_context=context)
+        gen_html('index', html_dir, chapter.markdown_file,
+                 generic_tmpl_name='chapter', extra_context=context)
 
 
 def gen_dir_from_markdown(source, output, generic_tmpl_name='content.jinja',
