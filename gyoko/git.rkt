f34d3d54f19657cda83ab4e71c680c2b7ce53074
Travis Jensen <travis@innerbrane.com>
2014-06-06 00:29:48 -0600

    Clean up git call. Convert checkout-site..

--- gyoko/git.rkt
+++ gyoko/git.rkt
@@ -1,57 +1,91 @@
-#lang racket
+#lang racket/base
+
+(require racket/system
+         racket/format
+         racket/string)
 
 (require "logger.rkt")
 
 (provide git)
 
 (define (clean-output bytes)
-  (string-split 
-   (string-replace (bytes->string/locale bytes) "\t" "    ") 
+  (string-split
+   (string-replace (bytes->string/locale bytes) "\t" "    ")
    "\n"))
 
-;; local-git is our current repository. It is the source used to pull
-;; from.
-(define local-git (find-system-path 'run-file))
+
+(define (find-git-base path)
+  (if (directory-exists? (build-path path ".git"))
+      path
+      (if (string=? (path->string path) "/")
+          (raise-user-error 'git 
+                            "unable to find local git directory")
+          (find-git-base (simplify-path (build-path path ".."))))))
+
+;; local-git is the git repository we are contained within.
+;; It is the source used to pull from.
+(define local-git (make-parameter (find-git-base (find-system-path 'run-file))))
+
 ;; working-git is the git repo that will be cloned into and that the
 ;; site will actually be generated in.
-(define build-git (build-path (find-system-path 'temp-dir) 
-                              (string-append "gyo" (~a (current-seconds)))))
+(define build-git (make-parameter
+                   (build-path (find-system-path 'temp-dir)
+                               (string-append "gyo" (~a (current-seconds))))))
 
 ;; Parameter that determines where (git) works.
-(define working-git (make-parameter local-git))
+(define working-git (make-parameter (local-git)))
 
-(define (git command arg-list)
-  (let ((full-args (if (> (log-level) 1) 
-                       (remove "--quiet" arg-list)
-                       arg-list))
-        (stdout-ob (open-output-bytes))
-        (stderr-ob (open-output-bytes))
-        (stdin-ib  (open-input-bytes #"")))
-    
-    (let* ((exit-code (parameterize ((current-output-port stdout-ob)
-                                     (current-error-port  stderr-ob)
-                                     (current-input-port  stdin-ib))
-                        (apply system*/exit-code 
-                               (find-executable-path "git") 
-                               "-C" (working-git) command 
-                               arg-list)))
-           (stdout-lines (clean-output (get-output-bytes stdout-ob)))
-           (stderr-lines (clean-output (get-output-bytes stderr-ob))))
+(define (git command . arg-list)
+  (let* ((full-args (append (list #"-C" (working-git) command)
+                            (if (> (log-level) 1)
+                                (remove #"--quiet" arg-list)
+                                arg-list)))
+         (stdout-ob (open-output-bytes))
+         (stderr-ob (open-output-bytes))
+         (stdin-ib  (open-input-bytes #""))
+         (exit-code (parameterize ((current-output-port stdout-ob)
+                                   (current-error-port  stderr-ob)
+                                   (current-input-port  stdin-ib))
+                      (apply system*/exit-code
+                             (find-executable-path "git") full-args)))
+         (stdout-lines (clean-output (get-output-bytes stdout-ob)))
+         (stderr-lines (clean-output (get-output-bytes stderr-ob))))
       (if (zero? exit-code)
           (begin
-            (debug "Output of git ~s:~n" command)
+            (debug "Output of git ~s~n" (apply ~a full-args))
             (log-lines DEBUG stdout-lines)
             (list stdout-lines stderr-lines))
           (begin
-            (info "Git command failed:~n~s ~s" command (string-join arg-list))
+            (info "Git command failed: git ~s ~s~n" command (apply ~a full-args))
             (info "stdout:~n")
             (log-lines INFO stdout-lines)
             (info "stderr:~n")
             (log-lines INFO stdout-lines)
-            (raise-user-error 'git "git command failed"))))))
+            (raise-user-error 'git "git command failed")))))
 
 
 (define (git-status)
-  (parameterize ([working-git (build-path "/Users/travis/src/uln")])
-    (git "status" '())))
+  (git #"status"))
 
+(define (local-up-to-date? branch)
+  (git #"remote" #"update")
+  (string=? (git #"rev-parse" branch)
+            (git #"rev-parse" (string-append "origin/" branch))))
+
+
+;; Prepare the temporary git repository for site generation
+(define (checkout-site branch)
+  (define (clear-checkout-tree)
+    (for ([f (directory-list (working-git))]
+          #:when (memf string=? (path->string f)
+                       (list ".git")))
+      (if (directory-exists? f)
+          (delete-directory f)
+          (delete-file f))))
+  (debug "testing that local repository is up to date~n")
+  (when (not (local-up-to-date? branch))
+    (raise-user-error 'git "Local repository doesn't match remote. Fix that first."))
+  (debug "cloning ~s to ~s~n" branch (build-git))
+  (parameterize ((working-git (build-git)))
+    (git #"clone" #"--quiet" #"--branch" branch (local-git) #".")
+    (clear-checkout-tree)))
