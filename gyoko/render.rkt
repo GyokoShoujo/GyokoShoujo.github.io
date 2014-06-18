5f784af561c0f56af7e0ad2e58c046a834fb621d
Travis Jensen <travis@innerbrane.com>
2014-06-17 22:55:12 -0600

    Add render.rkt for rendering mustache templates.

---
+++ gyoko/render.rkt
@@ -0,0 +1,14 @@
+#lang racket/base
+
+(require "logger.rkt")
+
+(provide render-template)
+
+(define (render-template src-path dest-path variable-hash)
+  (debug "Rendering ~s to ~s~n" src-path dest-path)
+  (call-with-output-file* dest-path
+                          (λ (port) 
+                           (define render 
+                             (dynamic-require src-path 'render))
+                           (render variable-hash port)) 
+                          #:mode 'text #:exists 'error))
