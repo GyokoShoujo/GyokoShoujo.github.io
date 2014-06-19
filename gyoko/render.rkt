cbdbfddf1ca8fe60c32e10646ddd179874f8fbea
Travis Jensen <travis@innerbrane.com>
2014-06-18 23:44:27 -0600

    render.rkt: Fix indentation of

--- gyoko/render.rkt
+++ gyoko/render.rkt
@@ -5,10 +5,10 @@
 (provide render-template)
 
 (define (render-template src-path dest-path variable-hash)
-  (debug "Rendering ~s to ~s~n" src-path dest-path)
+  (debug "Rendering ~s to ~s~n" src-path dest-path)    
   (call-with-output-file* dest-path
                           (λ (port) 
-                           (define render 
-                             (dynamic-require src-path 'render))
-                           (render variable-hash port)) 
+                            (define render 
+                              (dynamic-require src-path 'render))
+                            (render variable-hash port)) 
                           #:mode 'text #:exists 'error))
