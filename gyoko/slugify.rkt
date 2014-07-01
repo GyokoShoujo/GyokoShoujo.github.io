3ddb6e9105c9fd734742c41ec193da22f3fb0943
Travis Jensen <travis@innerbrane.com>
2014-06-30 21:59:35 -0600

    slugify.rkt: refactor and simplify.

--- gyoko/slugify.rkt
+++ gyoko/slugify.rkt
@@ -7,37 +7,28 @@
 
 (provide slugify)
 
-;; Converts a string that might have spaces and special characters into
-;; a string that only has ascii characters.
+(define (slugify-next s acc last)
+  (if (null? s)
+      (list->string (reverse (flatten acc)))
+      (let* ((c  (car s))
+             (sc (cond
+                  [(or (char<=? #\a c #\z)
+                       (char<=? #\0 c #\9)
+                       (char=? c #\-))        c]
+                  [(char<=? #\A c #\Z)        (char-downcase c)]
+                  [(or (char-whitespace? c)
+                       (char-blank? c))       #\-]
+                  [else                       #\.]))) ;; denotes "non-printable"
+        ;; If non-printable or a duplicate dash, don't add it to the list
+        (if (or (char=? sc #\.) (char=? sc last #\-))
+            (slugify-next (cdr s) acc last)
+            (slugify-next (cdr s) (cons sc acc) sc)))))
+
 
 (define (slugify str)
-  (letrec ((a (char->integer #\a))
-           (z (char->integer #\z))
-           (A (char->integer #\A))
-           (Z (char->integer #\Z))
-           (zero (char->integer #\0))
-           (nine (char->integer #\9))
-           (slugify-next
-            (λ (s acc last)
-              (if (null? s)
-                (list->string (reverse (flatten acc)))
-                (begin
-                  (let* ((c (car s))
-                         (v (char->integer c))
-                         (sc (cond
-                              [(or (and (>= v a) (<= v z))
-                                   (and (>= v zero) (<= v nine))
-                                   (eq? v #\-))         c] 
-                             [(and (>= v A) (<= v Z))   (char-downcase c)]
-                             [(or (char-whitespace? c)
-                                  (char-blank? c))      #\-]
-                             [else                      #\.])))
-                    (if (or (eq? sc #\.)           ;; Should we ignore this character? 
-                            (and (eq? sc last)     ;; non-ascii, space or duplicate '-'
-                                 (eq? last #\-)))  
-                        (slugify-next (cdr s) acc last)
-                        (slugify-next (cdr s) (cons sc acc) sc))))))))
-    (slugify-next (string->list str) null #f)))
+  ;; Converts a string that might have spaces and special characters into
+  ;; a string that only has ascii characters.
+  (slugify-next (string->list str) null #\.))
 
 
 (module+ test
@@ -45,4 +36,3 @@
   (check-equal? (slugify "3 - I'm a what?") "3-im-a-what")
   (check-equal? (slugify "Why am I here?") "why-am-i-here")
   (check-equal? (slugify "Why am I here? ΆΩ That's why.") "why-am-i-here-thats-why"))
-
