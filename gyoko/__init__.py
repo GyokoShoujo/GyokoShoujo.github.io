b811f32add6c788c09afbeccfa1656e61f1741b0
Travis Jensen <travis@innerbrane.com>
2014-08-27 23:58:28 -0600

    Remove old python code.

--- gyoko/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ['checkout_site', 'set_working_dir', 'gen_site',
-           'commit_site', 'push_site', 'GyokoException', ]
-
-from .base import set_working_dir, checkout_site, \
-    commit_site, push_site, did_site_change
-from .site import gen_site
-from .exceptions import GyokoException
--- gyoko/exceptions.py
+++
@@ -1,5 +0,0 @@
-
-class GyokoException(Exception):
-    '''
-    Exception raised if something goes wrong and is properly logged about.
-    '''
